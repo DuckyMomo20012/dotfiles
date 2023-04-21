6477ad65c72ad2fdeeceef1b7868dace4e5eaa0a
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-21 23:16:57 +0700

    chore: update asdf plugins version

--- asdf/nodejs.setup.sh
+++ asdf/nodejs.setup.sh
@@ -8,6 +8,6 @@ info "Installing NodeJS..."
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf install nodejs 18.14.12
+asdf install nodejs 18.16.0
 
-asdf global nodejs 18.14.12
+asdf global nodejs 18.16.0
