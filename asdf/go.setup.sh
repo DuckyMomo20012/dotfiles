02c5f5f4f96be09d9bc8fc007e4c37f1dd4c10e6
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-23 18:05:13 +0700

    fix: update asdf golang setup

--- asdf/go.setup.sh
+++ asdf/go.setup.sh
@@ -6,7 +6,7 @@ info() {
 
 info "Installing Golang..."
 
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 
 asdf install golang 1.20.3
 
