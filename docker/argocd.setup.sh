27908b35923fede1129c4c5387bfd34e27028630
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-06-15 23:01:53 +0700

    feat(docker): add argocd setup script

---
+++ docker/argocd.setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+docker_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# We have cd to directory to make sure find only searches in this directory
+cd "$docker_dir"
+
+# Load predefined functions
+. ../utils/functions.sh
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# Setup bash completion
+if [[ -f "$HOME/.bashrc" ]]; then
+    info "Setup argocd autocompletion for .bashrc..."
+
+    pushLineNonDup "if command -v argocd >/dev/null 2>&1; then source <(argocd completion bash); fi" "$HOME/.bashrc"
+fi
+
+if [[ -f "$HOME/.zshrc" ]]; then
+    info "Setup argocd autocompletion for .zshrc..."
+
+    pushLineNonDup "if command -v argocd >/dev/null 2>&1; then source <(argocd completion zsh); fi" "$HOME/.zshrc"
+fi
