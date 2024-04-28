077a683f512524bd84e04b901974a2627562881d
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-28 22:57:08 +0700

    feat(misc): fix audio sound

--- misc/setup.sh
+++ misc/setup.sh
@@ -3,3 +3,7 @@
 # Fix time conflict
 echo "Fixing time conflict..."
 sudo timedatectl set-local-rtc 1
+
+# Fix audio click/popping sound
+echo "Fix audio click/popping sound..."
+echo "options snd_hda_intel power_save=0" | sudo tee -a /etc/modprobe.d/audio_disable_powersave.conf
