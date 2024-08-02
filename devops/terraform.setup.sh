254875adf00b145626ab69517a5acf27ee4cf18b
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-08-02 14:59:23 +0700

    fix(devops, terraform): update install script

--- devops/terraform.setup.sh
+++ devops/terraform.setup.sh
@@ -13,7 +13,7 @@ gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com buster main" | \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update
