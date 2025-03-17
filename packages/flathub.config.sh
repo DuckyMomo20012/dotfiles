d7d42f223d374ec164db0ca5b87b6712d9b6d6af
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-03-17 09:29:27 +0700

    feat(packages): add flathub setup

---
+++ packages/flathub.config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# NOTE: Setup for Flathub
+
+sudo apt install flatpak -y
+
+sudo apt install gnome-software-plugin-flatpak -y
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
