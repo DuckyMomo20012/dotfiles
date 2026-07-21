a5cd4c288941dfa63b330f6a454178671548242f
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2026-07-21 11:59:55 +0700

    refactor: update bootstrap and install scripts

---
+++ bootstrap.ts
@@ -0,0 +1,99 @@
+import type { ParseArgsOptionsConfig } from 'node:util'
+import { join, resolve } from 'node:path'
+import process from 'node:process'
+import { parseArgs } from 'node:util'
+import { gpgDecrypt, gpgImport, logger, runSetupFilesInDir, symlinkDotfileDir } from './scripts/utils'
+
+const DEfAULT_DEDOT = false
+
+const helpMessage = `
+Usage: bun bootstrap.ts [options]
+
+Options:
+  -h, --help          Show this help message and exit
+  -d, --dedot         Run the dedot script to decrypt files (default: ${DEfAULT_DEDOT})
+`
+
+const options = {
+  help: {
+    type: 'boolean',
+    short: 'h',
+  },
+  dedot: {
+    type: 'boolean',
+    short: 'd',
+    default: DEfAULT_DEDOT,
+  },
+} as const satisfies ParseArgsOptionsConfig
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options,
+  strict: true,
+  allowPositionals: true,
+})
+
+if (values.help) {
+  console.log(helpMessage)
+  process.exit(0)
+}
+
+const isDedot = values.dedot ?? DEfAULT_DEDOT
+
+logger.info('Installing asdf...')
+await runSetupFilesInDir('./asdf', { runMainSetupOnly: false })
+
+logger.info('Running setup files...')
+
+// NOTE: Install setup files in the current directory, ignoring any setup files
+// in the "asdf" directory
+await runSetupFilesInDir('.', { runMainSetupOnly: false, ignores: ['asdf'] })
+
+const DOTFILES_DIR = process.env.DOTFILES_DIR ?? './dotfiles'
+
+logger.info(`Symlinking dotfiles from "${DOTFILES_DIR}" to home directory...`)
+await symlinkDotfileDir(DOTFILES_DIR, process.env.HOME ?? '')
+
+if (isDedot) {
+  logger.info('Importing GPG key...')
+
+  // NOTE: Prompt the gpg secret file to import to gpg
+  process.stdout.write('Please enter the path to your GPG secret file (e.g., secret.asc) (default: secret.asc): ')
+
+  const gpgFile = (await Bun.stdin.text()).trim() || 'secret.asc'
+
+  // NOTE: Prompt the email associated with the GPG key
+  process.stdout.write('Please enter the email associated with your GPG key: ')
+
+  const email = (await Bun.stdin.text()).trim()
+
+  if (!email) {
+    logger.error('Email is required to import the GPG key.')
+    process.exit(1)
+  }
+
+  await gpgImport(email, gpgFile)
+
+  const encryptedFile = join(resolve(__dirname, './private'), 'encrypted.tar.gz.gpg')
+
+  logger.info(`Decrypting encrypted file: ${encryptedFile}...`)
+
+  // NOTE: Prompt the password to decrypt the files
+  process.stdout.write('Please enter the passphrase to decrypt the file: ')
+
+  const passphrase = (await Bun.stdin.text()).trim()
+
+  const privateDir = resolve(__dirname, './private')
+
+  logger.info(`Decrypting ${encryptedFile} to ${privateDir}...`)
+
+  await gpgDecrypt(encryptedFile, privateDir, passphrase)
+
+  logger.info(`Symlink private dotfiles from "${privateDir}" to home directory...`)
+
+  await symlinkDotfileDir(privateDir, process.env.HOME ?? '')
+
+  logger.info(`Running private setup files from "${privateDir}"...`)
+
+  await runSetupFilesInDir(privateDir, { runMainSetupOnly: false })
+}
